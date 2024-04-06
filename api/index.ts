eca64133f6fe2743c4b460ccb6d738b26d3a8994
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-04-06 11:44:00 +0600

    Create mySqlDb and nodemon

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import mySqlDb from './mySqlDb';
 
 const app = express();
 const port = 8000;
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const run = async () => {
+  await mySqlDb.init();
+
   app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
   });
